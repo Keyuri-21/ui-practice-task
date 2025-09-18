27f3b8f40556fa4ee7877aa0648b790cb7990a36
Keyuri Patel <keyurip@zignuts.com>
2025-09-18 14:30:23 +0530

    created react project and basic set-up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {  
+
+  return (
+    <>
+      <div style={{borderRadius: 40, width: '1728px', height: '5371px'}}>
+        <div className='rectangle-1' style={{marginTop: '21px', width: '100%', height: '1048px', background: 'linear-gradient(180deg, #FCFCF7 6.5%, #F5EEAD 100%)'}}>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
