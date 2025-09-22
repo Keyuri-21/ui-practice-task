7d3ef4a0bbed8cbc7952a3e4bd9ce75fc86841a4
Keyuri Patel <keyurip@zignuts.com>
2025-09-22 08:13:11 +0530

    Added header and hero section in one container

---
+++ src/components/Rectangle-1/Rectangle-1.jsx
@@ -0,0 +1,14 @@
+import './Rectangle-1.scss';
+import Header from '../Header/Header';
+import HeroSection from '../HeroSection/HeroSection';
+
+const Rectangle1 = () => {
+  return (
+    <div className="rectangle-1">
+      <Header />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Rectangle1;
