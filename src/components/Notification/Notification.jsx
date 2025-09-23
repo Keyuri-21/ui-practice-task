09fec636ea37e9fa56ad364f274c76acda71d26d
Keyuri Patel <keyurip@zignuts.com>
2025-09-23 10:56:28 +0530

    Added notification badge for cart

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,32 @@
+import './Notification.scss';
+import { useCart } from '../../context/CartContext';
+
+const Notification = () => {
+  const { notification } = useCart();
+
+  if (!notification) return null;
+
+  return (
+    <div className="notification">
+      <div className="notification-content">
+        <svg 
+          className="notification-icon" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+          />
+        </svg>
+        <span>{notification}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
